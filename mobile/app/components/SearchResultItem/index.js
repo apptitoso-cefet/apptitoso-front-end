6d84c5e7b64418183ad8da027734c9edc3b23785
Paulo Vitor Barreto <pvitoreu@gmail.com>
2019-08-28 20:24:08 -0300

    Install react-native-vector-icons package

--- mobile/app/components/SearchResultItem/index.js
+++ mobile/app/components/SearchResultItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Image, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import style from './style'
 
@@ -10,6 +11,7 @@ const SearchResultItem = (props) => {
         <View style={{...style.container, ...props.style}}>
             <Image resizeMode='contain' style={style.image} source={imageSrc} />
             <Text style={style.text}>{name}</Text>
+            <Icon name='chevron-right'/>
         </View>
     )
 }
--- mobile/index.js
+++ mobile/index.js
@@ -6,8 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-if (__DEV__) {
-    require('react-devtools');
-}
-
 AppRegistry.registerComponent(appName, () => App);
