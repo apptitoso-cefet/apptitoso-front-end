f0368a5ab529398378f815d7d0cfd424bd1651d6
Ana Luísa Rogério Pimenta <anapimentainf@gmail.com>
2019-11-14 04:52:24 -0300

    lero lero do conceito culinario

--- mobile/app/screens/CulinaryConceptScreen/index.js
+++ mobile/app/screens/CulinaryConceptScreen/index.js
@@ -5,24 +5,7 @@ import ImageAboutField from '../../components/ImageAboutField'
 import TextScrollField from '../../components/TextScrollField'
 import { style } from './style'
 
-const LOREM_IPSUM = `Lorem ipsum dolor sit amet,
-consectetur adipiscing elit.
-Donec facilisis leo quam.
-Phasellus eget nisi pretium, tincidunt magna gravida, ultrices est.
-Morbi nec massa odio. Integer posuere purus et dictum euismod.
-Integer pulvinar purus nec commodo accumsan.
-Sed sit amet semper risus. Aenean a dui ac mauris ultricies interdum.
-Integer scelerisque ipsum id laoreet tempus.
-Ut lacus diam, vehicula vitae eros ut, feugiat finibus nunc
-Lorem ipsum dolor sit amet,
-consectetur adipiscing elit.
-Donec facilisis leo quam.
-Phasellus eget nisi pretium, tincidunt magna gravida, ultrices est.
-Morbi nec massa odio. Integer posuere purus et dictum euismod.
-Integer pulvinar purus nec commodo accumsan.
-Sed sit amet semper risus. Aenean a dui ac mauris ultricies interdum.
-Integer scelerisque ipsum id laoreet tempus.
-Ut lacus diam, vehicula vitae eros ut, feugiat finibus nunc`
+const LOREM_IPSUM = `Em gastronomia denomina-se al dente ao estado de cozimento da massa (e ocasionalmente do arroz) que, estando cozida, oferece alguma resistência (firmeza) ao ser mordida.[1] A massa fica firme mas não dura. A denominação prove/provem do italiano, no que a expressão significa precisamente ao dente. Pode aplicar-se por extensão às verduras cozidas, que devem ficar algo "estaladiças" - ou ao menos oferecer alguma resistência quando são mordidas. É com frequência considerada a forma ideal de cozimento da massa; em qualquer caso faz que a massa se mantenha firme, sendo isto especialmente importante nos pratos de massa "ao forno" (ao forno).`
 
 const CulinaryConceptScreen = ({ navigation }) => {
     return (
