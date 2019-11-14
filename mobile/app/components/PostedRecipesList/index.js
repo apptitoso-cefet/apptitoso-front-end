a9c975feabb811385ebb325bdb12cd972856841a
Paulo Vitor Barreto <pvitoreu@gmail.com>
2019-11-14 00:57:39 -0300

    Modify proptypes from postedRecipesList

--- mobile/app/components/PostedRecipesList/index.js
+++ mobile/app/components/PostedRecipesList/index.js
@@ -3,55 +3,13 @@ import { FlatList, View } from 'react-native'
 import { Text } from 'react-native-paper'
 import RecipeListItem from '../RecipeListItem'
 import style from './style'
-
-const listData = [
-    {
-        key: 'a',
-        name: 'Bolo de Cenouras',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'b',
-        name: 'Bom de Queijo',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'c',
-        name: 'Frutas Cristalizadas',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'd',
-        name: 'Gelo',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'e',
-        name: 'Bolo Decorativo',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'f',
-        name: 'Suco Funcional de Abacaxi',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'g',
-        name: 'Água Gelada',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-    {
-        key: 'h',
-        name: 'Bolo Verde',
-        imageSrc: require('../../assets/placeholderPhoto.jpg'),
-    },
-]
+import PropTypes from 'prop-types'
 
 const PostedRecipesList = props => {
     return (
         <View style={{ ...style.container, ...props.style }}>
             <FlatList
-                data={listData}
+                data={props.data}
                 renderItem={({ item }) => (
                     <RecipeListItem name={item.name} imageSrc={item.imageSrc} />
                 )}
@@ -64,6 +22,10 @@ const PostedRecipesList = props => {
             />
         </View>
     )
+}
+
+PostedRecipesList.propTypes = {
+    data: PropTypes.object,
 }
 
 export default PostedRecipesList
--- mobile/app/screens/PostedRecipesScreen/index.js
+++ mobile/app/screens/PostedRecipesScreen/index.js
@@ -4,11 +4,54 @@ import MaterialHeader from '../../components/MaterialHeader'
 import PostedRecipesList from '../../components/PostedRecipesList'
 import style from './style'
 
+const listData = [
+    {
+        key: 'a',
+        name: 'Bolo de Cenouras',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'b',
+        name: 'Bom de Queijo',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'c',
+        name: 'Frutas Cristalizadas',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'd',
+        name: 'Gelo',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'e',
+        name: 'Bolo Decorativo',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'f',
+        name: 'Suco Funcional de Abacaxi',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'g',
+        name: 'Água Gelada',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+    {
+        key: 'h',
+        name: 'Bolo Verde',
+        imageSrc: require('../../assets/placeholderPhoto.jpg'),
+    },
+]
+
 const PostedRecipesScreen = props => {
     return (
         <View style={style.container}>
             <MaterialHeader style={style.header} />
-            <PostedRecipesList style={style.result} />
+            <PostedRecipesList style={style.result} data={listData} />
         </View>
     )
 }
