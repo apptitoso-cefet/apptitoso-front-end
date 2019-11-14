8d01fb1b264e45d0a60471d517f36137e28d8080
Paulo Vitor Barreto <pvitoreu@gmail.com>
2019-11-13 22:53:52 -0300

    Add saved recipes screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import { Provider as PaperProvider } from 'react-native-paper'
 import LoginScreen from './app/screens/HomeScreen'
+=======
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
+import SearchResultsScheen from './app/screens/About'
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'red',
+        secondary: 'blue',
+        primary: 'yellow',
+    },
+}
+>>>>>>> Stashed changes
 
 function App() {
     return (
